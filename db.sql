3debfcb882971ae2e195afc420db32303137a7b8
mariamitco <mmitjavilac@gmail.com>
2022-06-12 22:18:56 +0200

    sql update

--- db.sql
+++ db.sql
@@ -1,9 +1,113 @@
-CREATE TABLE `app_db`.`users`(
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `email` VARCHAR(50) NOT NULL ,
-    `password` VARCHAR(50) NOT NULL ,
-    `name` VARCHAR(50) NOT NULL ,
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: db:3306
+-- Tiempo de generación: 12-06-2022 a las 20:18:35
+-- Versión del servidor: 5.7.38
+-- Versión de PHP: 8.0.19
 
-    PRIMARY KEY (`id`), UNIQUE (`email`),
-    UNIQUE (`name`)
-) ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `app_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `messages`
+--
+
+INSERT INTO `messages` (`id`, `user`, `message`) VALUES
+(2, 'marc', 'oausdaosjdais'),
+(3, 'marc@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `name`) VALUES
+(1, 'maria@gmail.com', '1234', 'maria'),
+(3, 'marc@gmail.com', '1234', 'marc'),
+(9, 'teixi@gmail.com', '1234', 'teixi'),
+(10, 'teixi1@gmail.com', '1234', 'teixi1'),
+(11, 'teixi2@gmail.com', '1234', 'teixi2'),
+(12, 'teixi3@gmail.com', '1234', 'teixi3'),
+(14, 'teixi4@gmail.com', '1234', 'teixi4'),
+(38, 'mark.ramajo15@gmail.com', 'asdasd', 'asd'),
+(40, 'aaa@ff.com', 'aaaaa', 'asdasa'),
+(46, 'mark.ramajo15@gmgbfdail.com', 'asd', 'rr'),
+(47, 'aa@aa.com', 'aa', 'aa'),
+(48, 'q@q.q', 'asd', 'qq');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
